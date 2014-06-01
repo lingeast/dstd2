844e66de5fb89c8fa4b8b5bf9ed3b75a268afc89
lingeast <yidongl1@uci.edu>
2014-05-31 18:16:08 -0700

    Modify test script

---
+++ project/transaction/cmpl-out-run.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+killall make
+
+make runregistry & 
+sleep 2
+make runtm & 
+sleep 2
+make runrmflights & 
+sleep 2
+make runrmrooms &
+sleep 2
+make runrmcars &
+sleep 2
+make runrmcustomers &
+sleep 2	
+make runwc &
+sleep 2
+
+cd ../test.part2
+cp -f Client.java ../transaction
+
+cd ../lockmgr
+make
+
+cd ../transaction
+make clean
+make server
+make client
+
+cd ../test.part2
+export CLASSPATH=".:gnujaxp.jar"
+
+
+javac RunTests.java
+java  -DrmiPort=4444 RunTests MASTER.xml
+
+pidof rmiregistry
